d31ce17a69253a36b374ea54d6c3767ceb7d481c
kaelaclement <stephanie.kaela@gmail.com>
2020-05-19 15:53:59 +0100

    move BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
@@ -8,16 +8,14 @@ import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
+    <div>
+      <Navbar />
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </div>
   );
 }
 
