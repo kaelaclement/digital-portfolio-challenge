bb923fef5e51b1174631f88d4cc38903d0c379d1
kaelaclement <stephanie.kaela@gmail.com>
2020-05-20 11:53:53 +0100

    add styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<div>
+		<div className='nav'>
 			<ul>
 				<li><Link to='/'>Home</Link></li>
 				<li><Link to='/about'>About</Link></li>
