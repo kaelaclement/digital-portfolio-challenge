478a09df9c6ca0e2411723122b0af526268b1c75
kaelaclement <stephanie.kaela@gmail.com>
2020-05-21 11:51:57 +0100

    add links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,10 @@ const Navbar = () => {
 				<li><Link to='/about'>About</Link></li>
 				<li><Link to='/employment'>Employment</Link></li>
 				<li><Link to='/projects'>Projects</Link></li>
-				<li><Link to='/contact'>Contact</Link></li>
+				<li><a href='https://github.com/kaelaclement' target='_blank' rel='noopener noreferrer'>Github</a></li>
+				<li><a href='https://www.linkedin.com/in/kaela-clement/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></li>
+				<li><a href='http://kaelaclement.com' target='_blank' rel='noopener noreferrer'>Blog</a></li>
+				<li><a href='mailto:kaela.s.clement@gmail.com'>Email</a></li>
 			</ul>
 		</div>
 	)
