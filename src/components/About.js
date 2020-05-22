b5a40ad7f58a548477f6d2a037c0aa78f21f64f1
kaelaclement <stephanie.kaela@gmail.com>
2020-05-22 10:56:37 +0100

    update about content and styles

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
 	return (
 		<div className='about'>
-			<p>This will be an about page</p>
+			<h3>Apprentice DevOps Engineer with a people-focused background, and eye on progress, and a passion for problem solving.</h3>
+			<p>Having recently moved from San Francisco, I’m excited for the opportunities waiting for me in the UK. I’m at my best when working on a challenge, and when I’m able to help others. Programming is the perfect way to do both. I pick up new skills quickly, and learned to code largely independently, but I’m excited to work and grow within a team moving forward. From social media, to banking apps, to self care and symptom trackers, accessibility and equality can and should be driven by developers. I believe tech is the way forward, and want to add my voice to a more accessible future. In my spare time I’m a yoga student, writer, and avid home cook.</p>
 		</div>
 	)
 }
