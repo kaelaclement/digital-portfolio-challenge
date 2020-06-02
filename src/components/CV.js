04e93ec8b63518947e12e1ea20bab3ce64c6e662
kaelaclement <stephanie.kaela@gmail.com>
2020-06-02 16:19:28 +0100

    clean up cv component

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 const CV = () => {
-	// const [numPages, setNumPages] = useState(null);
-	// const [pageNumber, setPageNumber] = useState(1);
-
-	// function onDocumentLoadSuccess({ numPages }) {
-	// 	setNumPages(numPages);
-	// }
-
 	return (
 		<div className="cv">
 			<Document
 				file='/cv.pdf'
-			// onLoadSuccess={onDocumentLoadSuccess}
 			>
 				<Page pageNumber={1} width={800} />
 			</Document>
-			{/* <p>Page {pageNumber} of {numPages}</p> */}
 		</div>
 	);
 }
